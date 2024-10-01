019c119a96c944f688f9e4cb7ffacb1ca76d21c9
Thomas.Arlt <thomasarrault69@gmail.com>
2024-10-01 11:11:20 +0200

    p

---
+++ app/main.js
@@ -0,0 +1,8 @@
+import render_grid from './components/grid.js';
+console.log("entrer")
+// Main
+window. addEventListener('load', () => {
+const grid = document.getElementById("grid");
+render_grid(grid);
+
+});
