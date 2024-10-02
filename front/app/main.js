84cbc6416567708affecd00ff15399b8a46b70b3
Thomas.Arlt <thomasarrault69@gmail.com>
2024-10-02 09:55:07 +0200

    "Deleted front-end code files and updated index.html"

--- front/app/main.js
+++
@@ -1,8 +0,0 @@
-import Grid from './components/grid.js';
-import { Board } from '.models/board.js';
-// Main
-window. addEventListener('load', () => {
-const grid = new Grid(document.getElementById("grid"));
-const board = new Board();
-grid.render(board);
-});
